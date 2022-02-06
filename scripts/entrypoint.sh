db00e2920dbd7722752eac1def34d5a287e196aa
glserranv <gleiry@valoreo.com>
2022-02-06 17:39:19 -0600

    hi

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,5 +3,5 @@
 source /scripts/entrypoint.sh
 
 cd $GITHUB_WORKSPACE
-pre-commit run gitlint -c /config/.precommit-config.yaml --hook-stage commit-msg --commit-msg-filename .git/COMMIT_EDITMSG
-pre-commit run -c /config/.precommit-config.yaml -a
+pre-commit run gitlint --hook-stage commit-msg --commit-msg-filename .git/COMMIT_EDITMSG
+pre-commit run -a
